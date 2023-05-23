303080a392f7c969acb882839053372370039bcb
Maxime ROBIN <robin.maxime44@gmail.com>
2023-05-23 18:01:24 +0200

    Logo du header cliquable

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,12 +6,14 @@ import { NavLink } from "react-router-dom";
 function Header() {
     return (
         <header className="header">
-            <img
-                className="header__img"
-                src="/assets/logo/logo210w.png"
-                alt="Logo de Kasa"
-                srcSet="/assets/logo/logo420w.png 2x"
-            />
+            <NavLink to="/">
+                <img
+                    className="header__img"
+                    src="/assets/logo/logo210w.png"
+                    alt="Logo de Kasa"
+                    srcSet="/assets/logo/logo420w.png 2x"
+                />
+            </NavLink>
             <nav className="header__nav">
                 <NavLink to="/">Accueil</NavLink>
                 <NavLink to="/about">A Propos</NavLink>
