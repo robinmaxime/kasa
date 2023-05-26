336664cf6d450333fbe90375b57a707b95244874
Maxime ROBIN <robin.maxime44@gmail.com>
2023-05-26 13:48:29 +0200

    amelioration du Loader

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,29 +1,40 @@
 import { useContext } from "react";
 import GalleryItem from "../GalleryItem";
 import { AccomodationContext } from "../../context";
+import Loader from "../Loader";
 
 /**
  * Composant affichant la galerie des logements disponibles
  */
 function Gallery() {
-    const { accomodation, isError } = useContext(AccomodationContext);
-
-    return (
-        <div className="gallery">
-            {isError ? (
+    const { accomodation, isError, isLoading } =
+        useContext(AccomodationContext);
+    if (isLoading) {
+        return (
+            <div className="gallery gallery--loading">
+                <Loader />
+            </div>
+        );
+    } else if (isError) {
+        return (
+            <div className="gallery">
                 <p>Erreur de chargement des données</p>
-            ) : (
-                accomodation.map((item) => (
+            </div>
+        );
+    } else {
+        return (
+            <div className="gallery">
+                {accomodation.map((item) => (
                     <GalleryItem
                         key={`logement-${item.id}`}
                         accomodationId={item.id}
                         title={item.title}
                         cover={item.cover}
                     />
-                ))
-            )}
-        </div>
-    );
+                ))}
+            </div>
+        );
+    }
 }
 
 export default Gallery;
